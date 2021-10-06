f2f7f6e1fef7e68e0850472abd5c922dbe5a4ccf
Sumanth U R <Sumanthr26@gmail.com>
2021-10-06 20:29:01 +0530

    api fetching and printing translation on div box

--- app.js
+++ app.js
@@ -4,8 +4,29 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 btnTranslate.addEventListener("click",clickEventHandler);
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text)
+{
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error){
+    console.log("error occurred",error);
+    alert("Something is wrong with server, please try again later");
+}
+
 function clickEventHandler(){
     
     
-    outputDiv.innerText = "shshshshshshhshshshs" + txtInput.value;
+    
+    //calling server API for minion transaltion
+    var InputText = txtInput.value
+    fetch(getTranslationURL(InputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;})
+    .catch(errorHandler)
+    
 };
